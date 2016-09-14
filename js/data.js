d7c72d7519f5b5b0c915a92bf531db71b44ee78e
liuchang <liuc0819@163.com>
2016-09-14 09:01:02 +0800

    mistake fixed

--- js/data.js
+++ js/data.js
@@ -13,7 +13,7 @@ dataObj.prototype.draw = function(){
     var w = can1.width;
     var h = can1.height;
     ctx1.save();
-    ctx1.shadowBlue = "10";
+    ctx1.shadowBlur = "10";
     ctx1.shadowColor = "white";
     ctx1.fillStyle = "white";
     ctx1.fillText("score " + this.score, w * 0.5, 40);
